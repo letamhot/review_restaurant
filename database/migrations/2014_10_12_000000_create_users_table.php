8316421bb0bfd8b7c8b3fd6cdccafe4d535b9be0
phuongsai <phuonghoang.s21@gmail.com>
2020-05-05 08:45:05 +0700

    Users migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,10 +27,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('role_id')
-                ->references('id')->on('roles')
-                ->onDelete('cascade')
-        ;
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
