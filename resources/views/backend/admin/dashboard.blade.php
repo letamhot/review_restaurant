e47a14f369432f070d2e7a251dc8a9f153df5953
phuongsai <phuonghoang.s21@gmail.com>
2020-05-13 16:41:29 +0700

    Stisla Admin template - OK

--- resources/views/backend/admin/dashboard.blade.php
+++ resources/views/backend/admin/dashboard.blade.php
@@ -2,24 +2,15 @@
 
 @section('title','Dashboard')
 
-@push('css')
-<!-- JQuery DataTable Css -->
-@endpush
-
 @section('section_header')
     <h1>Dashboard</h1>
 @endsection
 
 @section('content')
     <h3>Content 1</h3>
-    @include('layouts.backend.partials.home')
+    @include('backend.layouts.partials.home')
 @endsection
 
 @section('section_body')
     <h3>Content 2</h3>
 @endsection
-
-@push('js')
-    <!-- JQuery DataTable JS -->
-    <script src="{{ asset('assets/backend/js/page/index-0.js') }}"></script>
-@endpush
---
+++ resources/views/backend/layouts/partials/error_page_search.blade.php
@@ -0,0 +1,24 @@
+<div class="page-search">
+  <!-- Search Form-->
+  <form action="" method="post">
+      @csrf
+      <div class="form-group floating-addon floating-addon-not-append">
+          <div class="input-group">
+              <div class="input-group-prepend">
+                  <div class="input-group-text">
+                      <i class="fas fa-search"></i>
+                  </div>
+              </div>
+              <input type="text" class="form-control" id="search" name="search" placeholder="Search">
+              <div class="input-group-append">
+                  <button type="submit" class="btn btn-primary btn-lg">
+                    Search
+                  </button>
+              </div>
+          </div>
+      </div>
+  </form>
+  <div class="mt-3">
+      <a href="#">Back to Home</a>
+  </div>
+</div>
